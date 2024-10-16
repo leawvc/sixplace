4c161263ec030495e28450c1df57c6ca82e5a7bf
igwangmin <eoeo777755@naver.com>
2024-10-16 21:03:56 +0900

    commit : first commit

---
+++ src/main/java/com/sixplace/SixplaceApplication.java
@@ -0,0 +1,13 @@
+package com.sixplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixplace/SixplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
